d9603107b248d97e0a0dba633018a52809d7edb6
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-21 03:06:43 +0500

    Update App2.js
    call Functions example

--- App2.js
+++ App2.js
@@ -1,11 +1,52 @@
+// console.log("Before Preparing Food !");
+
+// function prepareFood(){
+
+//     console.log("Preparing Food !");
+
+// }
+
+// prepareFood();
+
+// console.log("After Preparing Food; Dinner is ready !")
+
 console.log("Before Preparing Food !");
 
-function prepareFood(){
-
-    console.log("Preparing Food !");
-
+function prepareFood(callback){
+    setTimeout(()=>{
+        console.log("Preparing Food");
+        callback("Food is Ready !")
+    },10)
 }
 
-prepareFood();
+function prepareFrenchToast(callback){
+    setTimeout(()=>{
+        console.log("Preparing French Toast");
+        callback("French Toast is ready !")
+    },20)
+}
 
-console.log("After Preparing Food; Dinner is ready !")
+function prepareCoffee(callback){
+    setTimeout(()=>{
+        console.log("Preparing Coffee");
+        callback("Coffee is ready !")
+    },30)
+}
+
+function myCallback(value){
+    console.log("Food callback = ",value);
+    prepareCoffee(coffeeCallback);
+}
+
+function frenchToastCallback(value){
+    console.log("French Toast callback = ", value);
+    prepareFrenchToast(frenchToastCallback);
+}
+
+function coffeeCallback(value){
+    console.log("Coffee callback = ", value);
+}
+
+prepareFood(myCallback);
+
+console.log("After Preparing Food; Dinner is ready !");
