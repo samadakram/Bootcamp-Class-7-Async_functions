1c821c0176ca354ebfc38a6dcdb07b9837c4c7c5
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-22 02:04:34 +0500

    created  callbackHell.js
    in this file simple callback function is written in one function all other functions are called one after the other

---
+++ callbackHell.js
@@ -0,0 +1,40 @@
+console.log("Class will be Started at 9:00 am ");
+
+const classTimeTable = (callback) =>{
+    setTimeout( ()=>{
+        console.log("Class 1: Started => Programming");
+        callback("Web Development");
+    },2000);
+}
+
+const class_2 = (callback) => {
+    setTimeout( () => {
+        console.log("Class 2 Started => Web Development");
+        callback("DataBase Systems");
+    },4000);
+}
+
+const class_3 = (callback) => {
+    setTimeout( ()=>{
+        console.log("Class 3 Started => DataBase Systems");
+        callback("Off Time !");
+    },6000);
+}
+
+classTimeTable(myCallback);
+
+function myCallback(value){
+    console.log("Next Class is => ",value);
+    class_2(class_2_callback);
+}
+
+function class_2_callback(value){
+    console.log("Next Class is => ",value);
+    class_3(class_3_callback);
+}
+
+function class_3_callback(value){
+    console.log(" => ",value);
+}
+
+console.log("Classes Off Time !");
